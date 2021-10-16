75753a3df4f0f861e5197e49f133f006ddd3d070
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-10-16 08:05:04 -0700

    - installed styled-components - skeleton code for footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Navbar } from "./components";
+
 function App() {
   return (
     <div className="App">
-      <h1>Empty</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Nav } from "./Navbar.styles";
+
+const Navbar = () => {
+  return <Nav>Test this</Nav>;
+};
+
+export default Navbar;
